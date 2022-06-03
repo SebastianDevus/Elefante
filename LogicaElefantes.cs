3f42b5c7004df254d69c09d6ff3ad6b7296ea9e8
SebastianDevus <sebastiandevus@gmail.com>
2022-06-03 20:33:10 -0300

    Finalizando exercicio

--- LogicaElefantes.cs
+++ LogicaElefantes.cs
@@ -6,4 +6,35 @@ public class LogicaElefantes
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine($"\n{mensagemDeErro}");
     }
+
+    public static void ExibeElefantes(int elefantes)
+    {
+        int contador = 1;
+        int contador2 = 1;
+
+        while (contador <= elefantes)
+        {
+            if (contador == 1) // Verifica quantidade para concordância verbal
+            {
+                Console.WriteLine($"{contador++} elefante incomoda muita gente");
+            }
+            else
+            {
+                Console.WriteLine($"{contador++} elefantes incomodam muita gente");
+            }
+
+            Console.Write($"{contador} elefantes ");
+
+            while (contador2 <= contador)
+            {
+                Console.Write("incomodam ");
+                contador2++;
+            }
+            contador2 = 1; 
+
+            Console.WriteLine("muito mais\n");
+            contador++;
+        }
+        return;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,30 +1,30 @@
-﻿int contador = 0;
-int elefantes;
+﻿int elefantes;
 
 Console.Clear();
 Console.Write("Quantos elefantes? ");
 elefantes = Convert.ToInt32(Console.ReadLine());
+
 try
 {
     if (elefantes < 3) // Verifica se tem 2 elefantes ou menos
     {
         throw new Exception("São necessários mais do que 2 elefantes.");
     }
-    else
+    else if (elefantes % 2 != 0) // Verifica se é impar
     {
-        if (elefantes % 2 != 0) // Verifica se é impar
-        {
-            throw new Exception("O número de elefantes deve ser par.");
-        }
-        else // Se chegou aqui, temos um número par de elefantes maior que 2
-        {
-            // Adicionar elefantes
-        }
+        throw new Exception("O número de elefantes deve ser par.");
     }
+    else // Se chegou aqui, temos um número par de elefantes maior que 2
+    {
+        Console.WriteLine();
+        LogicaElefantes.ExibeElefantes(elefantes);
+    }
+
 }
-catch(Exception e)
+catch (Exception e)
 {
     LogicaElefantes.ExibeMensagemErro(e.Message);
+    Console.WriteLine(); // Espaçamento
 }
 finally
 {
